aff5d6cb0372a3497b80a51bba9ebfdc10a7090c
Joao Borchoski <joaohenriqueborchoski>
2022-09-08 09:41:09 -0300

    entrega

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "",
-        price: 0.00
+let body = document.querySelector("body")
+let header = document.createElement("header")
+let main = document.createElement("main")
+let h1 = document.createElement("h1")
+let section = document.createElement("section")
+let span1 = document.createElement("span")
+let span2 = document.createElement("span")
+let ul = document.createElement("ul")
+let section2 = document.createElement("section")
+let span3 = document.createElement("span")
+let span4 = document.createElement("span")
+let but = document.createElement("button")
+
+h1.innerText = "Virtual Market"
+span1.innerText = "Item"
+span2.innerText = "Valor"
+span3.innerText = `Total`
+span4.innerText = "R$ ?"
+but.innerText = "Finalizar compra"
+
+body.append(header, main)
+header.appendChild(h1)
+main.append(section, ul, section2, but)
+section.append(span1, span2)
+section2.append(span3, span4)
+
+header.classList.add("header")
+main.classList.add("main")
+section.classList.add("section")
+section2.classList.add("section2")
+ul.classList.add("ul")
+but.classList.add("but")
+
+function carrinho(lista){
+    for(let i = 0; i < lista.length; i++){
+        let li = document.createElement("li")
+        let p = document.createElement("p")
+        let p2 = document.createElement("p")
+        li.id = lista[i].id
+        p.innerText = lista[i].name
+        p2.innerText = `R$ ${lista[i].price}`
+        li.append(p, p2)
+        ul.append(li)
+        li.classList.add("li")
     }
-]
+    return ul
+}
+const but2 = document.querySelector(".but")
+
+function soma(){
+    let valor = 0
+    for(let i = 0; i < productsCart.length; i++){
+        valor = productsCart[i].price + valor 
+    }
+    span4.innerText = `R$ ${valor}`
+    return valor
+}
+
+but2.addEventListener("click", soma)
+
+carrinho(productsCart)
---
+++ produtos.js
@@ -0,0 +1,27 @@
+const productsCart = [
+    {
+        id: 0,
+        name: "Uva Crinsom",
+        price: 45
+    },
+    {
+        id: 1,
+        name: "Vinho Canção",
+        price: 18
+    },
+    {
+        id: 2,
+        name: "Água de Coco",
+        price: 9
+    },
+    {
+        id: 3,
+        name: "Mamão",
+        price: 10
+    },
+    {
+        id: 4,
+        name: "Água Tonica",
+        price: 18
+    }
+]
